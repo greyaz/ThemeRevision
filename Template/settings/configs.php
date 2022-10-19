6b92e4719d78cfbc58a273c04401809b9a0c7120
greyaz <greyaz@gmail.com>
2022-10-19 12:02:53 +0800

    update to 1.1.3

--- Template/settings/configs.php
+++ Template/settings/configs.php
@@ -52,7 +52,7 @@
                     <?= e('Override default fonts with "Google Fonts". <b>Only one font family name</b> supported by Google can be filled in for each category. Note: the font family name of a font may differ from it\'s general name.') ?><br>
                     <?= t('If this feature is not working, please check the CSP settings on your server first.') ?> <a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy" target="_blank" style="border-bottom:1px dotted var(--color-greyscale-3);""><?= t('More') ?></a><br>
                 </small></p>
-                <div class="panel" style="border-left:var(--style-border);">
+                <div class="panel" style="border-left:var(--style-border-thk);">
                     <label><b><?= t('UI') ?></b></label>
                     <p><small><?= t('A font name for Most parts of the system UI. Example: ') ?><i>Noto Sans</i></small></p>
                     <input type='text' value="<?= $configs['google_fonts']['ui'] ?>" name="google_fonts[ui]" placeholder="<?= t('Name, case-sensitive') ?>"/>
